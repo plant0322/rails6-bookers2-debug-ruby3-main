755ba95c37e411969bf3c09202ecd96102df9ae0
EC2 Default User <ec2-user@ip-172-31-31-207.ap-northeast-1.compute.internal>
2024-03-16 06:17:49 +0000

    [Fix]いいねとコメントの非同期化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,14 +2,16 @@ class BookCommentsController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = @book.id
-    comment.save
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    unless @book_comment.save
+      render 'error'  # app/views/book_comments/error.js.erbを参照
+    end
   end
 
   def destroy
-    @book_comment = BookComment.find(params[:id])
-    @book_comment.destroy
+    @book = Book.find(params[:book_id])
+    BookComment.find(params[:id]).destroy
   end
 
   private
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,11 +4,13 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
+    # app/views/favorites/create.js.erbを参照
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
+    # app/views/favorites/destroy.js.erbを参照
   end
 end
