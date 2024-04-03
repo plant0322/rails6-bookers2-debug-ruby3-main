03304b241baf860b8e36b63789cbc74897903715
plant0322 <159232279+plant0322@users.noreply.github.com>
2024-04-03 13:02:18 +0000

    [Check]ログの修正

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,8 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
+  has_many :notifications, as: :notifiable, dependent: :destroy
+
   has_many :book_comments, dependent: :destroy
 
 
@@ -9,6 +11,12 @@ class Book < ApplicationRecord
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
+  end
+
+  after_create do
+    user.followers.each do |follower|
+      notifications.create(user_id: follower.id)
+    end
   end
 
   def self.search_for(word, method)
